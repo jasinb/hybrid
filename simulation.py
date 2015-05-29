b6f1fc1e5c6df7f04b1481f277e2a93d97d8d409
Jasin Bushnaief <jasin@umbra3d.com>
2015-05-29 15:39:02 +0300

    Hacked and reverse engineered phoenix websocket protocol

--- simulation.py
+++ simulation.py
@@ -3,10 +3,43 @@ import time
 import random
 import json
 
+class Channel(object):
+    def __init__(self, conn):
+        self.ws = create_connection(conn)
+        self.ref = 0
+
+    def _makeref(self):
+        self.ref = self.ref + 1
+        return self.ref
+
+    def join(self, chan):
+        d = json.dumps({
+            'event': 'phx_join',
+            'topic': chan,
+            'ref': self._makeref(),
+            'payload': {}
+            })
+        self.ws.send(d)
+        self.chan = chan
+        print self.ws.recv()
+
+
+    def send(self, event, payload):
+        d = json.dumps({
+            'topic': self.chan,
+            'event': event,
+            'payload': payload,
+            'ref': self._makeref()
+            })
+        self.ws.send(d)
+        # print self.ws.recv()
+
 class Agent(object):
     def __init__(self, name):
         self.name = name
         self.state = 'idle'
+        self.chan = Channel('ws://localhost:4000/ws')
+        self.chan.join('agents:')
 
     def launch_computation(self, duration):
         self.state = 'computing'
@@ -28,6 +61,8 @@ class Agent(object):
             # idle, launch a new computation
             self.launch_computation(random.uniform(1, 100))
 
+        self.chan.send('agent_update', self.get_state())
+
     def get_state(self):
         s = { 'agent' : self.name }
         if self.state == 'computing':
@@ -35,23 +70,19 @@ class Agent(object):
         s['state'] = self.state
         return s
 
-ws = create_connection('wss://echo.websocket.org')
+
 
 agents = []
 for i in range(5):
     agents.append(Agent('Worker_{}'.format(i)))
 
+ref = 1
+
 while True:
     for a in agents:
-        time.sleep(random.uniform(0.01, 0.5))
+        time.sleep(random.uniform(0.1, 1.5))
         a.update_state()
-        print json.dumps(a.get_state())
+        s = a.get_state()
+        print '{}: {}{}'.format(s['agent'], s['state'], (' (%.1f%%)' % (100.0 * s['progress'])) if s['state'] == 'computing' else '')
     print 
     print 
-
-    # data = 'Foobar equals hoax'
-    # print 'Sending "{}"'.format(data)
-    # ws.send(data)
-    # result = ws.recv()
-    # print 'Received "{}"'.format(result)
-    # ws.close()
